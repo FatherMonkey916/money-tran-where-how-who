3041a5e6a127bb62e5eb894cc8f92d986c059e88
FatherMonkey916 <keyeffort.tosuccess@gmail.com>
2025-03-20 04:07:16 +0900

    finish:nodemailer

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import nodemailer from 'nodemailer';
 
 // Define the mail options interface
@@ -10,26 +11,30 @@ interface MailOptions {
 
 // Create a transporter for sending emails
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  host: "smtp.mailersend.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
     user: "focochat5@gmail.com",
-    pass: "Redmond$222", // Consider using environment variables for sensitive data
+    pass: "ummc vvjj lmhd pcfe", // Consider using environment variables for sensitive data
+  },
+  tls: {
+    ciphers:'SSLv3'
   },
 });
 
 // Function to send mail
 async function sendMailer(mailOptions: MailOptions): Promise<any> {
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent successfully:', info);
-    return info;
-  } catch (error) {
-    console.error('Error in nodemailer:', error);
-    throw error;
-  }
+  return new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+              console.error("Error in nodemailer:",err);
+              reject(err)
+          }
+          resolve(info)
+      })
+  })
 }
+
 
 export default sendMailer;
